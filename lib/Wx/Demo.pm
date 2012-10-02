e85c651c14efa024ab1d415962da65d6f010c661
Mark Dootson <mdootson@cpan.org>
2012-10-02 12:31:48 +0000

    Import of MDOOTSON/Wx-Demo-0.19 from CPAN.
    gitpan-cpan-distribution: Wx-Demo
    gitpan-cpan-version:      0.19
    gitpan-cpan-path:         MDOOTSON/Wx-Demo-0.19.tar.gz
    gitpan-cpan-author:       MDOOTSON
    gitpan-cpan-maturity:     released

--- lib/Wx/Demo.pm
+++ lib/Wx/Demo.pm
@@ -4,7 +4,7 @@
 ## Author:      Mattia Barbon
 ## Modified by:
 ## Created:     20/08/2006
-## RCS-ID:      $Id: Demo.pm 3308 2012-05-31 02:30:15Z mdootson $
+## RCS-ID:      $Id: Demo.pm 3409 2012-10-02 03:34:10Z mdootson $
 ## Copyright:   (c) 2006-2011 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
@@ -93,15 +93,15 @@ use Module::Pluggable::Object;
 
 use Wx::Demo::Source;
 
-our $VERSION = '0.18';
+our $VERSION = '0.19';
 
 __PACKAGE__->mk_ro_accessors( qw(tree widget_tree events_tree source notebook left_notebook failwidgets) );
 __PACKAGE__->mk_accessors( qw(search_term ) );
 
 #if( Wx::wxMAC()) {
     #Modern Mac defaults look better than our settings
-    #Wx::SystemOptions::SetOptionInt('window-default-variant', wxWINDOW_VARIANT_SMALL);
-    #Wx::SystemOptions::SetOptionInt('mac.listctrl.always_use_generic', 1);
+#   Wx::SystemOptions::SetOptionInt('window-default-variant', wxWINDOW_VARIANT_SMALL);
+#   Wx::SystemOptions::SetOptionInt('mac.listctrl.always_use_generic', 1);
 #}
 
 sub new {
